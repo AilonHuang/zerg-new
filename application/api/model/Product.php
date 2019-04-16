fd4dfd1558dcd41801f474b3da3845fbea33bb70
AilonHuang <529678737@qq.com>
2019-04-16 22:52:37 +0800

    最近新品接口编写

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -4,5 +4,18 @@ namespace app\api\model;
 
 class Product extends BaseModel
 {
-    //
+    protected $hidden = ['delete_time', 'main_img_id', 'pivot', 'from', 'category_id', 'create_time', 'update_time'];
+
+    public function getMainImgUrlAttr($value, $data)
+    {
+        return $this->prefixImgUrl($value, $data);
+    }
+
+    public static function getMostRecent($count)
+    {
+        $products = self::order('create_time', 'desc')
+            ->limit($count)
+            ->select();
+        return $products;
+    }
 }
---
+++ application/api/validate/Count.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\api\validate;
+
+class Count extends BaseValidate
+{
+    /**
+     * 定义验证规则
+     * 格式：'字段名'	=>	['规则1','规则2'...]
+     *
+     * @var array
+     */	
+	protected $rule = [
+	    'count' => 'isPositiveInteger|between:1,15'
+    ];
+    
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名'	=>	'错误信息'
+     *
+     * @var array
+     */
+    protected $message = [
+        'count' => 'count 必须是正整数'
+    ];
+}
