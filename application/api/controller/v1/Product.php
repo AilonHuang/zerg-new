2a83cc73b7273045ca5115690cfb6f0006a4cf70
AilonHuang <529678737@qq.com>
2019-04-17 22:30:20 +0800

    分类商品接口编写

--- application/api/controller/v1/Product.php
+++ application/api/controller/v1/Product.php
@@ -4,6 +4,7 @@ namespace app\api\controller\v1;
 
 use app\api\model\Product as ProductModel;
 use app\api\validate\Count;
+use app\api\validate\IDMustBePostiveInt;
 use app\lib\exception\ProductException;
 use think\Controller;
 
@@ -17,6 +18,17 @@ class Product extends Controller
             throw new ProductException();
         }
 
+        $products->hidden(['summary']);
+        return $products;
+    }
+
+    public function getAllInCategory($id)
+    {
+        (new IDMustBePostiveInt())->goCheck();
+        $products = ProductModel::getProductByCategoryID($id);
+        if ($products->isEmpty()) {
+            throw new ProductException();
+        }
         $products->hidden(['summary']);
         return $products;
     }
