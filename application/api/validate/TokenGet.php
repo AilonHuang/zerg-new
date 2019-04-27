ab63403e545ed1aba1d5382387026db5050f97a7
AilonHuang <529678737@qq.com>
2019-04-27 13:46:18 +0800

    实现 toke 身份权限体系

---
+++ application/api/validate/TokenGet.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace app\api\validate;
+
+
+class TokenGet extends BaseValidate
+{
+    /**
+     * 定义验证规则
+     * 格式：'字段名'	=>	['规则1','规则2'...]
+     *
+     * @var array
+     */
+    protected $rule = [
+        'code' => 'require|isNotEmpty'
+    ];
+
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名'	=>	'错误信息'
+     *
+     * @var array
+     */
+    protected $message = [
+        'code' => '没有 code 还想获取 token，做梦哦'
+    ];
+}
