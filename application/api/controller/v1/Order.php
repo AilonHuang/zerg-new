a9eea272c5be91c925ccecd63bbd124eba56d315
AilonHuang <529678737@qq.com>
2019-06-07 19:08:25 +0800

    历史订单

--- application/api/controller/v1/Order.php
+++ application/api/controller/v1/Order.php
@@ -6,12 +6,36 @@ use app\api\controller\BaseController;
 use app\api\service\Order as OrderService;
 use app\api\service\Token as TokenService;
 use app\api\validate\OrderPlace;
+use app\api\validate\PagingParameter;
+use app\api\model\Order as OrderModel;
 
 class Order extends BaseController
 {
     protected $beforeActionList = [
-        'checkExclusiveScope' => ['only'=>'placeOrder'],
+        'checkExclusiveScope' => ['only' => 'placeOrder'],
+        'checkPrimaryScope' => ['only' => 'getSummaryByUser'],
     ];
+
+    public function getSummaryByUser($page = 1, $size = 15)
+    {
+        (new  PagingParameter())->goCheck();
+        $uid = TokenService::getCurrentUID();
+        $pagingOrders = OrderModel::getSummaryByUser($uid, $page, $size);
+        if ($pagingOrders->isEmpty()) {
+            return [
+                'data' => [],
+                'current_page' => $pagingOrders->getCurrentPage(),
+            ];
+        }
+
+        $data = $pagingOrders->hidden(['snap_items', 'snap_address', 'prepay_id'])
+            ->toArray();
+        return [
+            'data' => $data,
+                'current_page' => $pagingOrders->getCurrentPage(),
+            ];
+
+    }
 
     public function placeOrder()
     {
--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -7,4 +7,13 @@ class Order extends BaseModel
     protected $hidden = ['user_id', 'delete_time', 'update_time'];
 
     public $autoWriteTimestamp = true;
+
+    public static function getSummaryByUser($uid, $page = 1, $size = 15)
+    {
+        $pagingData = self::where('user_id', '=', $uid)
+            ->order('create_time', 'desc')
+            ->paginate($size, true, ['page' => $page]);
+
+        return $pagingData;
+    }
 }
