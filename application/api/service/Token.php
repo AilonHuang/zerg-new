d85adfa67eef40b93072522387b27df8fcac4202
AilonHuang <529678737@qq.com>
2019-06-08 15:54:39 +0800

    获取用户地址接口

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -7,8 +7,8 @@ namespace app\api\service;
 use app\lib\enum\ScopeEnum;
 use app\lib\exception\ForbiddenException;
 use app\lib\exception\TokenException;
-use think\Cache;
 use think\Exception;
+use think\facade\Cache;
 use think\facade\Request;
 
 class Token
